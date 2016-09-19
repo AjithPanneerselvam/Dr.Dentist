746bfe052e4742f154402742ed45d2b2170cc173
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2016-09-18 19:42:46 -0600

    Routing bugs

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dr.Dentist' });
 });
 
-router.get('/search', function(req, res, next) {
-  res.render('search', { title: 'Search' });
-});
+// router.get('/search', function(req, res, next) {
+//   res.render('search', { title: 'Search' });
+//   next();
+// });
+
+// router.get('/add', function(req, res, next) {
+//   res.render('add', { title: 'Add' });
+// });
 
 
 module.exports = router;
